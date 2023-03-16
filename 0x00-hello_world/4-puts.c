1a3ad721613eb049d2497850bd21aef519e01f5c
ismakr <akroutismail01@gmail.com>
2023-03-16 08:30:25 -0700

    4-0

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
-
+/**
+ * main - program prints str
+ * @void: Description of parameter x
+ * Description: prog that prints str using
+ * only puts fun
+(* stdio.h: Section description)*
+ * Return: this is a void fun
+ */
 int main(void)
 {
 
