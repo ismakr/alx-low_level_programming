9bb980a1cf5d65ded4c49f20b44087795d82b493
ismakr <akroutismail01@gmail.com>
2023-03-16 10:15:21 -0700

    5-0

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program prints str
+ * @void: Description of parameter x
+ * Description: prog that prints str using
+ * only printf fun
+(* stdio.h: Section description)*
+ * Return: this is a void fun
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
