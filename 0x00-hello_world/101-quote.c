3ccca41b46110cd9bd43b73a684a7d8a8733036a
ismakr <akroutismail01@gmail.com>
2023-03-16 15:24:26 -0700

    101-0

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - program prints to stderr
+ * @void: Description of parameter x
+ * Description: prog that prints str using
+ * only printf fun
+(* stdio.h: Section description)*
+ * Return: this is a void fun ret 1
+ */
+int main(void)
+{
+	fwrite("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+			59, 1, stderr);
+	return (1);
+}
