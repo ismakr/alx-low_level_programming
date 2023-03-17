ba8f811b6522733dfc4d7f728749a53d40744f50
ismakr <akroutismail01@gmail.com>
2023-03-17 15:59:12 -0700

    3-0

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - program prints alph
+ * @void: Description of parameter x
+ * Description: prog that prints str using
+ * only printf fun
+(* stdio.h: Section description)*
+ * Return: this is a void fun
+ */
+int main(void)
+{
+	char	alph;
+
+	alph = 'a';
+	while (alph <= 'z')
+	{
+		putchar(alph);
+		alph++;
+	}
+	alph = 'A';
+	while (alph <= 'Z')
+	{
+		putchar(alph);
+		alph++;
+	}
+	putchar('\n');
+	return (0);
+}
