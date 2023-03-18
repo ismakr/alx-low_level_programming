c874008edc9e30a7990e7b0409a345110c548eac
ismakr <akroutismail01@gmail.com>
2023-03-18 10:35:43 -0700

    102.0

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - program prints comb3
+ * @void: Description of parameter x
+ * Description: prog that prints str using
+ * only printf fun
+(* stdio.h: Section description)*
+ * Return: this is a void fun
+ */
+int main(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i <= 98)
+	{
+		j = i + 1;
+		while (j <= 99)
+		{
+			putchar(i / 10 + 48);
+			putchar(i % 10 + 48);
+			putchar(32);
+			putchar(j / 10 + 48);
+			putchar(j % 10 + 48);
+			putchar(44);
+			putchar(32);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
