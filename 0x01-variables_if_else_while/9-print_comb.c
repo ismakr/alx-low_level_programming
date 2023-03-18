08e5e512bcc68fd17f61e7baf5dca04016d1d12e
ismakr <akroutismail01@gmail.com>
2023-03-17 17:37:15 -0700

    9-0

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * main - program prints num
+ * @void: Description of parameter x
+ * Description: prog that prints str using
+ * only printf fun
+(* stdio.h: Section description)*
+ * Return: this is a void fun
+ */
+int main(void)
+{
+	int n;
+
+	n = 48;
+	while (n <= 57)
+	{
+		putchar(n);
+		if (n != 57)
+		{
+			putchar(44);
+			putchar(32);
+		}
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
