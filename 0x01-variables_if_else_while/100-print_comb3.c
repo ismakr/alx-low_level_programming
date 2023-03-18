960721c96ae1d681a5c205a2d190d803507bf7d0
ismakr <akroutismail01@gmail.com>
2023-03-18 05:18:56 -0700

    100-0

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - program prints two dig comb
+ * @void: Description of parameter x
+ * Description: prog that prints str using
+ * only printf fun
+(* stdio.h: Section description)*
+ * Return: this is a void fun
+ */
+int main(void)
+{
+
+	int i;
+	int j;
+
+	i = 48;
+	while (i <= 56)
+	{
+		j = i + 1;
+		while (j <= 57)
+		{
+			putchar(i);
+			putchar(j);
+			putchar(44);
+			putchar(32);
+			j++;
+		}
+		i++;
+	}
+	return (0);
+}
