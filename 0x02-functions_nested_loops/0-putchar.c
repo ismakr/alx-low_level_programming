afb498079c5a9b663adeba2691cebdb81440cdbb
ismakr <akroutismail01@gmail.com>
2023-03-21 08:52:44 -0700

    0-9

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,22 @@
-#include "_putchar.c"
 #include "main.h"
+/**
+ * main - prints str using only _putchar
+ * @void: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: (0)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
