d138900abd326f4db589b4dffc3ed6b3a9a7e266
ismakr <akroutismail01@gmail.com>
2023-03-21 07:46:09 -0700

    0-0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,32 @@
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+/**
+ * main - prints str using only _putchar
+ * @void: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: (0)
+ */
+int main(void)
+{
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
+}
