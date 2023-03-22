6b02cd7e088b378afe2c3722341e600280d79638
ismakr <akroutismail01@gmail.com>
2023-03-22 07:14:19 -0700

    3-0

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _islower - prints str using only _putchar
+ * @c: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: (0) if lower (1) if upper
+ */
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else if (c >= 65 && c <= 90)
+	{
+		return (0);
+	}
+}
