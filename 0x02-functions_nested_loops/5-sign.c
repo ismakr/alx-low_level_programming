17fc29c3e2d3e8a6eda9f23a82439fdb963d6bdc
ismakr <akroutismail01@gmail.com>
2023-03-22 08:09:43 -0700

    5-0

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_sign - prints str using only _putchar
+ * @n: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: (0)if n=0  (1)if n>0 (-1)if n<0
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		putchar('0');
+		return (0);
+	}
+	else if (n < 0)
+	{
+		putchar('-');
+		return (-1);
+	}
+}
