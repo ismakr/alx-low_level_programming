b0bc6c5ced35cbafce48eaa4581e4b160ea315ae
ismakr <akroutismail01@gmail.com>
2023-03-23 10:46:59 -0700

    2-0

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - mul two dig
+ * @a: Description of parameter x
+ * @b: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: a*b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,2 @@
 int _putchar(char c);
-int _isdigit(int c);
+int mul(int a, int b);
