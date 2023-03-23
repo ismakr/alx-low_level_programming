a056fbff14fc909846ac3fd4015b4aafb87e502d
ismakr <akroutismail01@gmail.com>
2023-03-23 15:39:47 -0700

    4-0

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_most_numbers - prints dig
+ * @void: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: dig from 0-9 but 2 4
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	i = 48;
+	while (i <= 57)
+	{
+		if (i != 2 || i != 4)
+		{
+			_putchar(i);
+		}
+		i++;
+	}
+}
