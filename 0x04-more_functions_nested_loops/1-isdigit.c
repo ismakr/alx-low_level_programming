8214b7c3ea74669685dc12cd5fdc4c91e35a6c2a
ismakr <akroutismail01@gmail.com>
2023-03-23 10:31:46 -0700

    1-0

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isdigit - prints str using only _putchar
+ * @c: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: (1) if its dig (0) oth
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 56)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
