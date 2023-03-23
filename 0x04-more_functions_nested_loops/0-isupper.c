7b43b5698d5bc76cb1ec9f252c7f6f28122bbee7
ismakr <akroutismail01@gmail.com>
2023-03-23 10:26:50 -0700

    0-0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _isupper - prints str using only _putchar
+ * @c: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: (1) if its upper (0) oth
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
