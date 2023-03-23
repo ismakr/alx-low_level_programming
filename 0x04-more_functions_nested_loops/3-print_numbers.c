fd0290af68c6ce43fc846e887a03cdaef2a4a302
ismakr <akroutismail01@gmail.com>
2023-03-23 10:52:40 -0700

    3-0

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_numbers - mul two dig
+ * @void: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: dig from 0-9
+ */
+void print_numbers(void)
+{
+	int i;
+
+	i = 48;
+	while (i <= 57)
+	{
+		_putchar(i);
+		i++;
+	}
+	_putchar(10);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,2 +1,2 @@
 int _putchar(char c);
-int mul(int a, int b);
+void print_numbers(void);
