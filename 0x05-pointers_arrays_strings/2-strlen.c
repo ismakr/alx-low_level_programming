c0124b15bf1815e036ef2254244afc544aa0f314
ismakr <akroutismail01@gmail.com>
2023-03-27 08:47:36 -0700

    2-0

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+/**
+ * _strlen - ret the length of str
+ * @s: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: int i
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s != '\0')
+	{
+		*s++;
+		i++;
+	}
+	return (i);
+}
