865dee1016f4fb6a103dd37a5d73b5683ec11fc9
ismakr <akroutismail01@gmail.com>
2023-03-27 11:31:59 -0700

    4-0

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+ * _strlen - ret the length of str
+ * @s: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: int i
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+/**
+ * print_rev - rev
+ * @s: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: void
+ */
+void print_rev(char *s)
+{
+	int i;
+	char swp;
+
+	i = strlen(s) - 1;
+	while (i >= 0 && s != '\0')
+	{
+		putchar(*(s + i))
+			i--;
+	}
+}
