55fa4bed07a29a860ac423a5f3c0a85b2c714e55
ismakr <akroutismail01@gmail.com>
2023-03-30 08:44:28 -0700

    5-0

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+/**
+ * string_toupper - cat str
+ * @str: parametre
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return:  0
+ */
+char *string_toupper(char *str)
+{
+	int i;
+
+	i = 0;
+	while (*(str + i) != '\0')
+	{
+		if (*(str + i) >= 97 && *(str + i) <= 122)
+		{
+			*(str + i) = *(str + i) - 32;
+		}
+		i++;
+	}
+}
