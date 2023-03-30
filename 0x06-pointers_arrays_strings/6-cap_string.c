5e616310dd01910df0778c4e577c3cd5dd31bc2c
ismakr <akroutismail01@gmail.com>
2023-03-30 15:18:10 -0700

    6-0

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+/**
+ * cap_string - cp frt car str
+ * @s: Description of parameter s
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: int dest
+ */
+char *cap_string(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*(s + i) != '\0')
+	{
+		if (*(s + i) == ';' || *(s + i) == ',' || *(s + i) == '.'
+				|| *(s + i) == '!' || *(s + i) == '?'
+				|| *(s + i) == '"' || *(s + i) == '(' || *(s + i) == ')'
+				|| *(s + i) == '{' || *(s + i) == '}' || *(s + i) == 10
+				|| *(s + i) == 32 || *(s + i) == 9)
+		{
+			if (*(s + i + 1) >= 97 && *(s + i + 1) <= 122)
+			{
+				*(s + i + 1) -= 32;
+			}
+		}
+		i++;
+	}
+}
