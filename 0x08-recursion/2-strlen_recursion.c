535dad5a93df10b3ce7754d74ececa6189ac7fbc
ismakr <akroutismail01@gmail.com>
2023-04-05 15:24:56 -0700

    2-0

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen_recursion - ret the length of str
+ * @s: Description of parameter x
+(* a blank line
+ * Description: Longer description of the function)?
+(* section header: Section description)*
+ * Return: int i
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return (0);
+	}
+	return (1 + _strlen(s + 1));
+}
