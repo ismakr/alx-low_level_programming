8f8e71186d6cf450f903c130fef95c6ea510b3d4
ismakr <akroutismail01@gmail.com>
2023-04-05 08:18:01 -0700

    0-0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,14 @@
+/**
+ * _puts_recursion - put str with recursion
+ * @s: pointer to ft ch
+ * return: void fun
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_putchar(*s);
+	_puts_recursion(s + 1);
+}
