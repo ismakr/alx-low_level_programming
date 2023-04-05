b3f89dc0f479d3f1f778722e838a132950d3d0c7
ismakr <akroutismail01@gmail.com>
2023-04-05 15:20:59 -0700

    1-1

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,31 +1,11 @@
 #include "main.h"
 /**
- * _strlen - ret the length of str
- * @s: Description of parameter x
-(* a blank line
- * Description: Longer description of the function)?
-(* section header: Section description)*
- * Return: int i
- */
-int _strlen(char *s)
-{
-	if (*s == '\0')
-	{
-		return (0);
-	}
-	return (1 + _strlen(s + 1));
-}
-/**
  * _print_rev_recursion - put str with recursion rev
  * @s: pointer to ft ch
  * return: void fun
  */
 void _print_rev_recursion(char *s)
 {
-	if (_strlen(s) == 0)
-	{
-		_putchar(10);
-	}
 	if (*s == '\0')
 	{
 		return;
