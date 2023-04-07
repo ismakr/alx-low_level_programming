338d8b607e346772426305132d198602bcf1bcdb
ismakr <akroutismail01@gmail.com>
2023-04-07 10:14:36 -0700

    0-0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * main - prog prints its name
+ * @argc: the number of arguments on the command line
+ * @argv: an array of C-style strings
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < argc)
+	{
+		j = 0;
+		while (*(*(argv + i) + j) != '\0')
+		{
+			_putchar(*(*(argv + i) + j));
+			j++;
+		}
+		i++;
+	}
+	_putchar(10);
+	return (0);
+}
