71da4e52b1c6a4ec784203bab680838e616ef3f6
ismakr <akroutismail01@gmail.com>
2023-04-07 10:57:30 -0700

    1-0

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+/**
+ * ft_putnb - prints nub
+ * @n: int
+ * Return: void
+ */
+void ft_putnb(int n)
+{
+	if (n < 0)
+	{
+		n = -n;
+		_putchar('-');
+	}
+	if (n < 10)
+	{
+		_putchar(n + 48);
+	}
+	else
+	{
+		ft_putnb(n / 10);
+		ft_putnb(n % 10);
+	}
+}
+/**
+ * main - prog prints its name
+ * @argc: the number of arguments on the command line
+ * @argv: an array of C-style strings
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	ft_putnb(argc);
+}
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -1,1 +1,2 @@
 int _putchar(char c);
+void ft_putnb(int n);
