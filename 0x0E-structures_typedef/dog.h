50ec7b4dc8bcaaa13109ba0423ce2f4e8a4c0054
ismakr <akroutismail01@gmail.com>
2023-04-17 10:58:24 -0700

    -

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,17 @@
-typedef struct dog{
-char * name;
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - struct tsk
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
+typedef struct dog
+{
+char *name;
 float age;
-char * owner;
+char *owner;
 }
+#endif
