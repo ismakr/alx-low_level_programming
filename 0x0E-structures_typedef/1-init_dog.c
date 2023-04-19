725ddea1226e561cefc1b4190e5d8546e621fcc5
ismakr <akroutismail01@gmail.com>
2023-04-18 17:47:09 -0700

    1-0

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,14 @@
+/**
+ * init_dog -  initialize a variable of type struct dog
+ * @d: pointer
+ * @name: pointer to str
+ * @age: float
+ * @owner: pointer to str
+ * Return: void
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
