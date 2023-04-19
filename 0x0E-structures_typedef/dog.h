efab5728ebb996663c8dddf33eba53eb99fd72c6
ismakr <akroutismail01@gmail.com>
2023-04-18 18:37:34 -0700

    3-1

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,25 +1,19 @@
 #ifndef dog_h
 #define dog_h
 /**
- * struct dog - struct tsk
+ * struct dog_t - struct tsk
  * @name: First member
  * @age: Second member
  * @owner: Third member
  *
  * Description: Longer description
  */
-typedef struct dog
-{
-char *name;
-float age;
-char *owner;
-} dog;
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-typedef struct dog
+typedef struct dog_t
 {
 char *name;
 float age;
 char *owner;
 } dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 #endif
