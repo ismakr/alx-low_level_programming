92b3066f1d42eafe4a548d3a3aee14af1d407192
ismakr <akroutismail01@gmail.com>
2023-04-30 16:10:09 -0700

    5-0

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+#include <stdlib.h>
+/**
+ * free_listint2 -  frees a listint_t list.
+ * @head: ptr
+ * Return: void
+ */
+void free_listint2(listint_t **head);
+{
+	listint_t *p;
+
+	p = head;
+	while (p != NULL)
+	{
+		p = p->next;
+		free(head);
+		head = p;
+	}
+	head = NULL;
+}
