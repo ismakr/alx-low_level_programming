4cb9f511218559094ab29542eea1895b2e9af334
ismakr <akroutismail01@gmail.com>
2023-04-30 23:59:54 -0700

    5-2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -10,10 +10,13 @@ void free_listint2(listint_t **head)
 	listint_t *p;
 
 	p = *head;
+	if (*head == NULL)
+		return;
 	while (p != NULL)
 	{
 		p = p->next;
 		free(*head);
 		*head = p;
 	}
+	*head = NULL;
 }
