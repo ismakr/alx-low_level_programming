d3155fc8896db42c6688977e9de1ceeac448f88b
ismakr <akroutismail01@gmail.com>
2023-05-02 01:51:27 -0700

    9-4

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -18,33 +18,22 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (node == NULL)
 		return (NULL);
 	p = *head;
-	while (p != NULL)
+	if (idx == 0 && i == 0)
 	{
+		node = p;
+		*head = node;
+		return (node);
+	}
+	while (i < (idx - 1))
+	{
+		if (p == NULL && p->next == NULL)
+			return (NULL);
 		p = p->next;
 		i++;
 	}
-	if (idx > i)
-		return (NULL);
-	p = *head;
-	i = 0;
-	while (p != NULL)
-	{
-		if ((idx - 1 == i) || (i == 0 && idx == 0))
-		{
-			tmp = p->next;
-			if (idx == 0)
-			{
-				node = p;
-				*head = node;
-			}
-			else
-				p->next = node;
-			node->n = n;
-			node->next = tmp;
-			break;
-		}
-		p = p->next;
-		i++;
-	}
+	tmp = p->next;
+	p->next = node;
+	node->next = tmp;
+	node->n = n;
 	return (node);
 }
