4778d543a66dde9ce22237f09d8b3b0457660d1f
ismakr <akroutismail01@gmail.com>
2023-05-02 23:51:55 -0700

    10-1

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -29,7 +29,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	i = 0;
 	while (i < index)
 	{
-		if (node == NULL)
+		if (node == NULL || node->next == NULL)
 			return (-1);
 		node = node->next;
 		if (i + 1 == index)
