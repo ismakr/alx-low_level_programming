ea8edbc679e4d94ca332542a3e069394c8e6f05b
ismakr <110137433+ismakr@users.noreply.github.com>
2023-05-04 10:24:52 +0100

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/1-pi.h
+++
@@ -1,4 +0,0 @@
-#ifndef object_like_macro_h
-#define object_like_macro_h
-#define PI 3.14159265359
-#endif
--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,4 +0,0 @@
-#ifndef function_like_macro_h
-#define function_like_macro_h
-#define ABS(b) ((b < 0) ? b * -1 : b)
-#endif
