6b2e760330e92a931c311f42f8ffa51ab2fdb424
ismakr <akroutismail01@gmail.com>
2023-06-19 13:59:03 -0700

    2-3

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -7,6 +7,8 @@
  */
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+		return;
 	if (d->name == NULL)
 		printf("Name: (nil)\n");
 	else
@@ -19,6 +21,4 @@ void print_dog(struct dog *d)
 		printf("Owner = (nil)\n");
 	else
 		printf("Owner = %s\n", d->owner);
-	if (d == NULL)
-		return;
 }
