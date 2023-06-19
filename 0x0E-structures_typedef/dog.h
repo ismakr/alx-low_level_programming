6845f52ab449f7b236205119ea8a932fe0e816af
ismakr <akroutismail01@gmail.com>
2023-06-19 11:22:58 -0700

    0-0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - desc
+ * @name: ptr to a str -dog name
+ * @age: float nbr dog -age
+ * @owner: ptr to a str -owner name
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
