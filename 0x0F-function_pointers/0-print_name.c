9e9d3c90e4f541144039edd64414508968922e1f
ismakr <akroutismail01@gmail.com>
2023-06-21 11:04:46 -0700

    0-0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+/**
+ * print_name -  prints a name. using ptr to fun
+ * @name: ptr to str name to be printed
+ * @f: ptr the fun that prints the name.
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
