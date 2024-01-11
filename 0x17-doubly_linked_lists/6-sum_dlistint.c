a68477368666ffe4a4997ee0560d88230d8a26de
ismakr <akroutismail01@gmail.com>
2024-01-11 15:06:12 -0800

    6-2

--- 0x17-doubly_linked_lists/6-sum_dlistint.c
+++ 0x17-doubly_linked_lists/6-sum_dlistint.c
@@ -11,14 +11,12 @@ int sum_dlistint(dlistint_t *head)
 	dlistint_t *ptr;
 	int sum;
 
-	if (head == NULL)
-		return (0);
 	sum = 0;
 	ptr = head;
-	while (ptr->next != NULL)
+	while (ptr != NULL)
 	{
-		ptr = ptr->next;
 		sum += ptr->n;
+		ptr = ptr->next;
 	}
 	return (sum);
 }
