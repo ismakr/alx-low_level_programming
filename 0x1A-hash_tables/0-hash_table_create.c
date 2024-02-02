e6794be1167efa9aecce393371624fe4579f0425
ismakr <akroutismail01@gmail.com>
2024-02-02 09:17:33 -0800

    0-0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,7 +18,11 @@ hash_table_t *hash_table_create(unsigned long int size)
 	h_table->array = malloc(sizeof(hash_node_t *) * size);
 	if (h_table->array == NULL)
 		return (NULL);
-	for (i = 0; i < size; i++)
+	i = 0;
+	while (i < size)
+	{
 		h_table->array[i] = NULL;
+		i++;
+	}
 	return (h_table);
 }
