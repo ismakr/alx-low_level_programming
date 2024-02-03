b8662b4dbda8f60c475823e48b61797276c1a5d4
ismakr <akroutismail01@gmail.com>
2024-02-03 14:31:27 -0800

    5-0

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,34 @@
+#include "hash_tables.h"
+/**
+ * hash_table_print - prints a hash table.
+ * @ht: hash table
+ * Return: void
+ */
+void hash_table_print(const hash_table_t *ht)
+{
+	hash_node_t *node;
+	unsigned long int i;
+	char print_flag;
+
+	print_flag = 0;
+	if (ht == NULL)
+		return;
+	i = 0;
+	printf("{");
+	while (i < ht->size)
+	{
+		node = ht->array[i];
+		while (node != NULL)
+		{
+			if (print_flag == 1)
+				printf(", ");
+			printf("'%s': '%s'", node->key, node->value);
+			node = node->next;
+			if (node != NULL)
+				printf(", ");
+			print_flag = 1;
+		}
+		i++;
+	}
+	printf("}\n");
+}
