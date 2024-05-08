143adbeb0807e6e623f80b4d7f165a87ef38ccb8
ismark <akroutismail01@gmail.com>
2024-05-08 13:43:36 -0700

    0-1

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,4 +4,5 @@
 int linear_search(int *array, size_t size, int value);
 void ft_putnbr(int nb);
 void ft_printf(char *format, size_t a, int b);
+int binary_search(int *array, size_t size, int value);
 #endif
